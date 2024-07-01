087f0a6fa614e964cce8f8347a7ae150ed024968
karthikchary <karthik.chary138@gmail.com>
2024-07-01 18:18:40 +0530

    Update the output code

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,21 +1,12 @@
 # outputs.tf
-output "credentials_csv" {
-  value = <<EOT
+data "template_file" "credentials_csv" {
+  template = <<EOT
 UserName,AccessKeyId,SecretAccessKey
 $${aws_iam_user.example_user.name},$${aws_iam_access_key.example_user_key.id},$${aws_iam_access_key.example_user_key.secret}
 EOT
-  sensitive = true
 }
 
-resource "null_resource" "write_credentials_csv" {
-  provisioner "local-exec" {
-    command = <<EOT
-echo "UserName,AccessKeyId,SecretAccessKey" > credentials.csv
-echo "$${aws_iam_user.example_user.name},$${aws_iam_access_key.example_user_key.id},$${aws_iam_access_key.example_user_key.secret}" >> credentials.csv
-EOT
-  }
-  triggers = {
-    user_name = aws_iam_user.example_user.name
-    access_key_id = aws_iam_access_key.example_user_key.id
-  }
+resource "local_file" "credentials_csv" {
+  content  = data.template_file.credentials_csv.rendered
+  filename = "$${path.module}/credentials.csv"
 }
